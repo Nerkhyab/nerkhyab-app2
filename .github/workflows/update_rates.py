e24374aca69aeb491156ef4c71d273b9c5a53cce
Nerkhyab <safibahadori980@gmail.com>
2026-02-17 08:12:59 +0330

    Create update_rates.py

---
+++ .github/workflows/update_rates.py
@@ -0,0 +1,8 @@
+from telethon import TelegramClient
+import json
+import os
+
+# اطلاعات تلگرام
+api_id = YOUR_API_ID          # جایگزین با API ID خودت
+api_hash = 'YOUR_API_HASH'    # جایگزین با API Hash خودت
+channel = 'NerkhYab_Khorasan' # نام کانال
